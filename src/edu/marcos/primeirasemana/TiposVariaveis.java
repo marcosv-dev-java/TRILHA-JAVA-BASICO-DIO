fd51644ca1ae8a021b4cf99509f65980356f5d19
MarcosP124 <junetube07@gmail.com>
2025-11-30 09:21:06 -0400

    Primeiro commit

---
+++ src/edu/marcos/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,8 @@
+package edu.marcos.primeirasemana;
+
+public class TiposVariaveis {
+    public static void main(String[] args){
+        String nome = "Marcos";
+        System.out.println("\033[1;31mMeu nome é "+ nome + " e estou aprendendo a progamar JAVA!!");
+    }
+}
