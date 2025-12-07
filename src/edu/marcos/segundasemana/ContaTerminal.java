59c010d9b1acd2313b4af7785a11a2104f255fc2
MarcosP124 <junetube07@gmail.com>
2025-12-07 10:01:06 -0400

    segunda semana

---
+++ src/edu/marcos/segundasemana/ContaTerminal.java
@@ -0,0 +1,34 @@
+package edu.marcos.segundasemana;
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+
+        System.out.println("\033[1;34m=== BEM VINDO AO BANCO TERMINAL ===\033[m");
+        System.out.println(" Digite o número da sua conta: ");
+        int numeroConta = input.nextInt();
+
+        System.out.println(" Digite a agência da sua conta: ");
+        String agenciaConta = input.next();
+
+        System.out.println(" Digite o seu nome cadastrado: ");
+        String nomeCliente = input.next();
+
+        System.out.println(" Digite seu saldo bancário: ");
+        double saldo = input.nextDouble();
+
+
+        System.out.println(" Processando informações...");
+
+        System.out.println(" ACESSANDO CONTA DE " + nomeCliente);
+        System.out.println(" Conta: " + numeroConta);
+        System.out.println(" Agência: " + agenciaConta);
+        System.out.println(" Saldo: \033[1;32m" + saldo+"\033[m");
+
+
+    }
+}
