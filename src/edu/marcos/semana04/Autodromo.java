a0ac32722e30f534da651a469490f4176cdaaa06
MarcosP124 <junetube07@gmail.com>
2025-12-27 23:13:12 -0400

    Iniciando a POO e arquivo UML adicionado na semana 4

---
+++ src/edu/marcos/semana04/Autodromo.java
@@ -0,0 +1,11 @@
+package edu.marcos.semana04;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.confereCombustivel();
+        jeep.confereCambio();
+        jeep.ligar();
+
+    }
+}
---
+++ src/edu/marcos/semana04/Carro.java
@@ -0,0 +1,14 @@
+package edu.marcos.semana04;
+
+public class Carro {
+    public void ligar(){
+        System.out.println("CARRO LIGADO");
+    }
+    public void confereCombustivel(){
+        System.out.println("CONFERINDO COMBUSTIVEL");
+
+    }
+    public void confereCambio(){
+        System.out.println("CONFERINDO CAMBIO EM P");
+    }
+}
