c09a7dde4a2450e48c72e095e60e9afe9da9dcdc
Marcos <marcosv.java@gmail.com>
2026-01-10 23:22:49 -0400

    Teste

---
+++ src/edu/marcos/semana06/dados/No.java
@@ -0,0 +1,29 @@
+package edu.marcos.semana06.dados;
+
+public class No {
+    private int dado;
+    private No refNo = null;
+
+    public No(int dado) {
+        this.dado = dado;
+    }
+
+    public No() {
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+}
