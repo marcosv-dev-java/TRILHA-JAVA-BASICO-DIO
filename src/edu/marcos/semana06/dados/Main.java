d6900fb67753a2427f78d15e1c289383c5a88d50
Marcos <marcosv.java@gmail.com>
2026-01-11 11:19:02 -0400

    alterações no projeto pilha e novo arquivo semana 7

---
+++ src/edu/marcos/semana06/dados/Main.java
@@ -0,0 +1,20 @@
+package edu.marcos.semana06.dados;
+
+public class Main {
+    public static void main(String[] args){
+        Pilha minhaPilha = new Pilha();
+
+        minhaPilha.push(new No(1));
+        minhaPilha.push(new No(2));
+        minhaPilha.push(new No(3));
+        minhaPilha.push(new No(4));
+        minhaPilha.push(new No(5));
+        minhaPilha.push(new No(6));
+        minhaPilha.push(new No(7));
+
+        System.out.println(minhaPilha);
+
+
+
+    }
+}
--- src/edu/marcos/semana06/dados/Pilha.java
+++ src/edu/marcos/semana06/dados/Pilha.java
@@ -25,4 +25,18 @@ public class Pilha {
     public boolean isEmpty(){
         return refNoEntradaPilha == null ? true : false;
     }
+    public String toString(){
+        String stringRetorno = "-----------------\n";
+        stringRetorno += "       Pilha\n";
+        stringRetorno += "-----------------\n";
+        No noAuxiliar = refNoEntradaPilha;
+        while (true){
+            if(noAuxiliar != null){
+                stringRetorno += "[No{dado="+ noAuxiliar.getDado()+"}]\n";
+                noAuxiliar = noAuxiliar.getRefNo();
+            }else break;
+        }
+        stringRetorno += "=================\n";
+        return stringRetorno;
+    }
 }
