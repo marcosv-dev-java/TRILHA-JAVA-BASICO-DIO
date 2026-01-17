df688da629cc570c84af14471c86f70107d22a4c
Marcos <marcosv.java@gmail.com>
2026-01-17 16:31:11 -0400

    Projeto Jogo finalizado

--- src/edu/marcos/semana07/jogo/Main.java
+++ src/edu/marcos/semana07/jogo/Main.java
@@ -8,7 +8,10 @@ public class Main {
         Poder jogador2 = null;
         Scanner sc = new Scanner(System.in);
         System.out.println("Bem vindo ao jogo");
-        System.out.println("Digite o poder do jogador 1: ");
+        for(int i = 1; i <= 2; i++){
+            // Criamos um objeto temporário para poder atribuir o valor do poder nos jogadores
+            Poder escolhaPoder;
+            System.out.println("Digite o poder do jogador " + i +":");
         while(true){
             System.out.print(
                     """
@@ -19,59 +22,62 @@ public class Main {
                             3- VOID\
                             
                             Digite sua escolha:\s""");
-            int escolha = sc.nextInt();
-            if(escolha == 1){
-                jogador1 = Poder.FOGO;
+            int opcao = sc.nextInt();
+            if(opcao == 1){
+                escolhaPoder = Poder.FOGO;
                 break;
             }
-            if(escolha == 2){
-                jogador1 = Poder.RAIO;
+            if(opcao == 2){
+                escolhaPoder = Poder.RAIO;
                 break;
             }
-            if(escolha == 3){
-                jogador1 = Poder.VOID;
-                break;
-            }
-            else System.out.println("\033[1;31mOPÇÃO INVÁLIDA\033[m");
-        } System.out.println("Digite o poder do jogador 2: ");
-        while(true){
-            System.out.print(
-                    """
-                            1- FOGO\
-                            
-                            2- RAIO\
-                            
-                            3- VOID\
-                            
-                            Digite sua escolha:\s""");
-            int escolha = sc.nextInt();
-            if(escolha == 1){
-                jogador2 = Poder.FOGO;
-                break;
-            }
-            if(escolha == 2){
-                jogador2 = Poder.RAIO;
-                break;
-            }
-            if(escolha == 3){
-                jogador2 = Poder.VOID;
+            if(opcao == 3){
+                escolhaPoder = Poder.VOID;
                 break;
             }
             else System.out.println("\033[1;31mOPÇÃO INVÁLIDA\033[m");
         }
+        // Se for a vez do jogador 1 escolher atribui para o jogador 1, se não, atribui para o jogador 2
+        if (i == 1){
+            jogador1 = escolhaPoder;
+        }else jogador2 = escolhaPoder;
+        }
+
         System.out.println("=== JOGO INICIADO ===");
         System.out.println("JOGADOR 1: " + jogador1.getTipoPoder());
         System.out.println("JOGADOR 2: " + jogador2.getTipoPoder());
         while(true){
-            while(jogador1.getHp() != 0  || jogador2.getHp() != 0){
+            while(jogador1.getHp()> 0 && jogador2.getHp()> 0){
+                for(int i = 1; i <= 2; i++){
+                    Poder atacante = (i == 1) ? jogador1 : jogador2;
+                    Poder defensor = (i == 1) ? jogador2 : jogador1;
+                    if(atacante.getHp() <= 0 ||defensor.getHp() <= 0) break;
+                    System.out.println("Turno do jogador "+ i);
+                    System.out.println("""
+                            1 - Atacar
+                            2 - Ver HP Inimigo""");
+                    int escolha = sc.nextInt();
+                    sc.nextLine();
+                    if (escolha == 1){
+                        System.out.println("==========================");
+                        System.out.println("Jogador "+ i + " atacou o jogador " + (i == 1 ? "2": "1") + " e causou " + atacante.getDano()+ " de Dano");
+                        atacante.carregarUltimate();
+                        if (atacante.podeUsarUltimate()){
+                            atacante.executarUltimate(defensor);
+                        }
+                        defensor.receberDano(atacante.getDano());
+                        System.out.println(defensor.toString());
+                    }
+                    else if  (escolha == 2) System.out.println("\033[1;32mHP: \033[m" + defensor.getHp());
+                    System.out.println("==========================");
 
+                }
 
             }
-
+            if (jogador1.getHp() <= 0 || jogador2.getHp() <= 0) break;
         }
-
-
-
+        System.out.println("=== JOGO FINALIZADO ===");
+        System.out.println(jogador1.getHp() > 0 ? "Jogador 1 venceu" : "Jogador 2 venceu");
 
 
     }
--- src/edu/marcos/semana07/jogo/Poder.java
+++ src/edu/marcos/semana07/jogo/Poder.java
@@ -1,19 +1,19 @@
 package edu.marcos.semana07.jogo;
 
 public enum Poder {
-    FOGO(200,"\033[1;31mFOGO\033[m", 800){
+    FOGO(200,"\033[1;31mFOGO\033[m", 1400){
         public void executarUltimate(Poder inimigo){
             System.out.println("Inimigo queimou e perdeu \033[1;31m70hp\033[m!");
             inimigo.receberDano(70);
         }
     },
-    RAIO(150,"\033[1;33mRAIO\033[m", 650){
+    RAIO(150,"\033[1;33mRAIO\033[m", 1700){
         public void executarUltimate(Poder inimigo){
             System.out.println("Inimigo levou um raio de alta potência!! perdeu \033[1;31m120hp\033[m!");
             inimigo.receberDano(120);
         }
     },
-    VOID(400, "\033[1;35mVOID\033[m", 300){
+    VOID(400, "\033[1;35mVOID\033[m", 900){
         public void executarUltimate(Poder inimigo){
             System.out.println("INIMIGO FOI SUGADO POR 2 PORTAIS!! perdeu \033[1;31m400hp\033[m!");
             inimigo.receberDano(400);
@@ -33,6 +33,13 @@ public enum Poder {
 
     public int getDano() {
         return dano;
+    }
+    private int jogadas;
+    public int carregarUltimate(){
+        return jogadas++;
+    }
+    public boolean podeUsarUltimate(){
+        return carregarUltimate() == 3;
     }
 
 
