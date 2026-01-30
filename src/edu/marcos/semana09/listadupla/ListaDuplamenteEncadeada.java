41bd763a46e66613966f18dd35636fce70376d7b
Marcos <marcosv.java@gmail.com>
2026-01-29 22:22:11 -0400

    Adicionando metodos size() e getNo() e construtor na Lista Duplamente Encadeada

---
+++ src/edu/marcos/semana09/listadupla/ListaDuplamenteEncadeada.java
@@ -0,0 +1,32 @@
+package edu.marcos.semana09.listadupla;
+
+public class ListaDuplamenteEncadeada<T> {
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+    private int tamanhoLista = 0;
+
+    public ListaDuplamenteEncadeada() {
+        this.tamanhoLista = 0;
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+    }
+
+    public int size(){
+        return tamanhoLista;
+    }
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+
+    }
+
+
+
+
+
+
+}
