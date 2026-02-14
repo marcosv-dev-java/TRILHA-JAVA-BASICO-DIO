f9aeaad66a8b3701e2d5e5da7f478e02ff6c8306
Marcos <marcosv.java@gmail.com>
2026-02-14 15:35:06 -0400

    implementacao da classe No da estrutura de dados Arvore

---
+++ src/edu/marcos/semana11/tree/BinaryNode.java
@@ -0,0 +1,45 @@
+package edu.marcos.semana11.tree;
+
+public class BinaryNode<T extends Comparable<T>> { // qualquer objeto que for definido nesse No precisa ter a interface comparable implementada.
+    private T content;
+    private BinaryNode<T> left;
+    private BinaryNode<T> right;
+
+    public BinaryNode(T content) {
+        this.content = content;
+        this.left = this.right = null;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public BinaryNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryNode<T> left) {
+        this.left = left;
+    }
+
+    public BinaryNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryNode<T> right) {
+        this.right = right;
+    }
+
+
+    public String toString() {
+        return "BinaryNode{" +
+                "content=" + content +
+                '}';
+    }
+}
+
+
