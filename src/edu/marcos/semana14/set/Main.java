1a677ffd13440f04ee536fe8e390cd612387a0a6
Marcos <marcosv.java@gmail.com>
2026-03-08 08:56:27 -0400

    iniciando a semana 15 estudando collections framework

--- src/edu/marcos/semana14/set/Main.java
+++ src/edu/marcos/semana14/set/Main.java
@@ -1,11 +1,15 @@
 package edu.marcos.semana14.set;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
+
+
         Set<Carro> hashSetCarro = new HashSet<Carro>();
+
 
         hashSetCarro.add(new Carro("Fiat"));
         hashSetCarro.add(new Carro("Volkswagen"));
@@ -18,17 +22,12 @@ public class Main {
         Set<Carro> treeSetCarro = new TreeSet<Carro>();
 
 
-
         System.out.println(treeSetCarro);
 
         treeSetCarro.addAll(hashSetCarro);
         System.out.println(treeSetCarro);
         treeSetCarro.add(new Carro("BYD"));
         System.out.println(treeSetCarro);
-
-
-
-
 
 
     }
