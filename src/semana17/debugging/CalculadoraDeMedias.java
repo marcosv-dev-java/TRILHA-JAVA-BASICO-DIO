30e7e80f365c5e3002c0e3a7b4eb33f03077913b
Marcos <marcosv.java@gmail.com>
2026-04-18 10:18:41 -0400

    Entendendo a pilha de exeções e debugging na semana 17

---
+++ src/semana17/debugging/CalculadoraDeMedias.java
@@ -0,0 +1,24 @@
+package semana17.debugging;
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        String[] alunos = {"Pelézinho","Ronaldo","Felipe","Marcao Gostosão"};
+        double media = calcularMediaDaTurma(alunos,sc);
+        System.out.printf("Média da turma %.2f", media);
+
+
+    }
+    public static double calcularMediaDaTurma(String[] alunos, Scanner scan){
+        double soma = 0;
+        for(String aluno : alunos){
+            System.out.printf("Nota do aluno %s: ",aluno);
+            soma += scan.nextDouble();
+
+        }
+        return  soma /alunos.length;
+
+    }
+
+}
---
+++ src/semana17/debugging/Main.java
@@ -0,0 +1,29 @@
+package semana17.debugging;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Progama iniciado no main");
+        a();
+        System.out.println("Progama finalizado no main!!");
+
+    }
+    static void a(){
+        System.out.println("Entrou no método A!");
+        b();
+        System.out.println("Finalizou o método A!");
+
+    }
+    static void b(){
+        System.out.println("Entrou no método B!");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+        }
+        c();
+        System.out.println("Finalizou o método B!");
+    }
+    static void c(){
+        System.out.println("Entrou no método C!");
+        System.out.println("Finalizou o método C!");
+    }
+
+}
