7c3eef49203233b8ace296e6291fe6ae21afd0d6
Marcos <marcosv.java@gmail.com>
2026-04-20 10:24:07 -0400

    Fazendo desafio envolvendo caracterer e arquivo

---
+++ src/edu/marcos/semana18/IO/ExemploIOArquivo.java
@@ -0,0 +1,27 @@
+package edu.marcos.semana18.IO;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ExemploIOArquivo {
+    public static void main(String[] args) {
+        try {
+            lerArquivo();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void lerArquivo() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("arquivodetexto.txt"));
+        StringBuilder sb = new StringBuilder();
+        String linha;
+
+        while ((linha = br.readLine()) != null){
+            sb.append(linha);
+            sb.append("\n");
+        }
+        br.close();
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/edu/marcos/semana18/IO/ExercicioIOCaracter.java
@@ -0,0 +1,38 @@
+package edu.marcos.semana18.IO;
+
+import java.io.*;
+
+public class ExercicioIOCaracter {
+    public static void main(String[] args) {
+        try {
+            receberTeclado();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void receberTeclado() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Recomende 3 animes: ");
+        String linha = br.readLine();
+
+
+        /* Para impressão no console usando o System.out, o BufferedWriter se torna a pior opção
+        Por que sempre precisamos lembrar de dar bw.flush para os dados aparecer
+        o System.out.println faz isso sozinho, com o bw se torna mais verboso de forma desnecessária
+        Usamos mais o BufferedWriter quando tratamos de Arquivos, Fotos, arquivo de musica etc.
+         */
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do{
+            bw.write(linha);
+            bw.newLine();
+            linha = br.readLine();
+
+        }while(!(linha.isEmpty()));
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+}
